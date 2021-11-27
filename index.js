2f9fc832bae1ee3002646934ff38619c68bfe4ae
eightys3v3n <eightys3v3n@gmail.com>
2021-11-27 10:40:34 -0700

    return all paintings and by id

--- index.js
+++ index.js
@@ -9,25 +9,51 @@ const HOST = process.env.HOST;
 
 let paintings = [];
 
-function readData(path) {
-  let data = null;
-  
+function readPaintings(path) {
   fs.readFile(path, (err, data) => {
     if (err) {
-      console.warn(`Failed to read JSON file ${jsonPath}`);
+      console.warn(`Failed to read paintings JSON file ${jsonPath}`);
       console.warn(err);
     } else {
-      data = JSON.parse(data);
+      paintings = JSON.parse(data);
     }
   });
-
-  return data;
 }
 
-paintings = readData(JSON_PATH);
+readPaintings(JSON_PATH);
 
+app.use(express.static('static'));
+
+// return all paintings
 app.get("/", (req, res) => {
+  console.log(`Returning all paintings`);
+  res.setHeader('Content-Type', 'application/json');
   res.json(paintings);
+});
+
+// return single painting
+app.get("/:id", (req, res) => {
+  let resPaintings = paintings.filter(p => {
+    return p.paintingID == req.params.id;
+  });
+
+  if (resPaintings && resPaintings.length > 0) {
+    console.log(`Returning paintings by ID ${req.params.id}`);
+    res.json(resPaintings[0]);
+  } else {
+    res.send(`No paintings found by ID ${req.params.id}`);
+    console.log(`No paintings found by ID ${req.params.id}`);
+  }
+});
+
+// return all paintings in gallery
+app.get("/gallery/:id", (req, res) => {
+
+});
+
+// all paintings between min and max years
+app.get("/year/:min/:max", (req, res) => {
+
 });
 
 //app.get("/title/:id/:sub", (req, res) => {req.params.sub, req.params.id});
